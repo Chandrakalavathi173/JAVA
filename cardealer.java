4752f1540d59a50866d888214c2bbe08f366e13c
Chandrakalavathi173 <130962067+Chandrakalavathi173@users.noreply.github.com>
2023-07-06 09:47:51 +0530

    Update cardealer.java

--- cardealer.java
+++ cardealer.java
@@ -3,6 +3,13 @@ public class Shop {
 	private static int carcost;
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Available cars in the showroom : ");
+		String[] cars= {"Polo Trendline","Polo Highline","Virtus Trendline","Virtus Highline","Taigun Trendline","Taigun Highline","Taigun Topline"};
+		System.out.println();
+		for(int i=0;i<cars.length;i++) {
+			System.out.println(cars[i]);
+		}
+		System.out.println();
 		System.out.println("Select car model:");
 		String carname=sc.nextLine();
 		switch(carname) {
@@ -35,7 +42,6 @@ public class Shop {
 			System.out.println("Car cost : "+carcost);
 			break;
 		}
-		sc.nextLine();
 		System.out.println("Do you want insurance ?");
 		String insurance=sc.next();
 		
@@ -43,7 +49,7 @@ public class Shop {
 		String add_accessories=sc.next();
 		
 		System.out.println("Dealer Discount:");
-		int discount=sc.nextInt();
+		String discount=sc.next();
 		
 		final int RTO=113990;
 		final int TCS_charges=11000;
@@ -58,17 +64,26 @@ public class Shop {
 			add_accessories_amt=15000;
 			total+=add_accessories_amt;
 		}
-		double discount_amount=discount*carcost/100;
-		if(discount_amount>30000) {
-			System.out.println("Maximum discount applied is 30000");
+		int discount_amount;
+		if (discount.endsWith("%")) {
+			discount=discount.replace("%","");
+			discount_amount=Integer.valueOf(discount)*carcost/100;
 		}
-		if(!insurance.equals("yes") || !insurance.equals("YES") || !add_accessories.equals("yes") || !add_accessories.equals("YES") && discount_amount > 30000) {
+		else
+			discount_amount=Integer.valueOf(discount);
+		if(discount_amount>30000) {
+			System.out.println("Discount amount is "+discount_amount+" But Maximum discount applied is 30000");
+		}
+		System.out.println();
+		if(((!insurance.equalsIgnoreCase("yes") && !insurance.equalsIgnoreCase("YES")) && (!add_accessories.equalsIgnoreCase("yes") && !add_accessories.equalsIgnoreCase("YES"))) && discount_amount > 0) {
+			System.out.println("Any one of the additional features need to be added to access the discount!!");
 			discount_amount=0;
 			total-=discount_amount;
 		}
 		if(insurance.equals("yes") || insurance.equals("YES") || add_accessories.equals("yes") || insurance.equals("YES") && discount_amount <= 30000) {
 			total-=discount_amount;
 		}
+		System.out.println();
 		System.out.println("Total cost "+total+"("+carname+" "+carcost+" + "+RTO+"(RTO)"+insurance_amt+"(Insurance) + "+TCS_charges+"(TCS) + "+add_accessories_amt+"(Additional Accessories) - "+discount_amount+"(Dealer discount))");
 	}
 }
